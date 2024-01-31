b1ade818e059954997255d0df87ec476358d6734
Bradley Rushforth <bradleyrushforth@Bradleys-MacBook-Air.local>
2024-01-31 22:14:49 +0000

    Added a navbar, background colour, and a welcome message

--- education-platform/src/App.tsx
+++ education-platform/src/App.tsx
@@ -1,13 +1,21 @@
+// App.tsx
 import React from 'react';
 import BackgroundApp from './frontends/background/background';
-import './App.css';
+import Navbar from './frontends/navbar/navbar';
+import InfoComponent from './frontends/info/infoComponent';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
+    <div className='app-container'>
+      <InfoComponent />
       <BackgroundApp />
+      <Navbar className="navbar" activeIndex={0}>
+        <a href="#">Courses</a>
+        <a href="#">About</a>
+        <a href="#">Team</a>
+      </Navbar>
     </div>
   );
-}
+};
 
 export default App;
